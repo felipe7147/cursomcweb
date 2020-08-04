97cc251c09253620e042a2a167337336c5a9c26a
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-08-04 17:10:41 -0300

    Esqueci a senha

--- src/main/java/com/example/cursomc/config/SecurityConfig.java
+++ src/main/java/com/example/cursomc/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	};
 	
-	private static final String[] PUBLIC_MATCHERS_POST = { "/produtos/**", "/categorias/**"
+	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**", "/auth/forgot/**"
 
 	};
 
